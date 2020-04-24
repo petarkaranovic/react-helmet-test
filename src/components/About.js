48dcab61d255ab01a0d6f6c2fa8564922ea3466e
petarkaranovic <petarkaranovic@yahoo.com>
2020-04-24 15:12:35 +0200

    react snap added

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 
 const About = () => {
+    const [aboutText] = useState(`Morbi nec augue nisi. Sed dolor diam, consectetur ut consequat vel, facilisis vel mi. Vestibulum 
+    ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse nec urna id nulla aliquet tristique. 
+    Morbi mattis felis in dignissim volutpat. Fusce varius varius libero egestas porta. Aliquam eu tincidunt ante. Duis scelerisque elementum nisi, 
+    non dignissim velit lacinia eu. Duis elit elit, condimentum eget purus eu, egestas consectetur leo. Sed sit amet neque et velit rutrum porttitor. 
+    In non rhoncus sem. Cras non orci eros. Pellentesque non diam commodo, tincidunt dui quis, vestibulum dui. Proin aliquet posuere tempus`);
     return (
         <div>
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>I am from About</title>
                 <link rel="canonical" href="http://localhost:3000/about" />
-                <meta name="description" content="This is a react-helmet demo app and we are currently in About page" />
+                <meta name="description" content={aboutText.slice(0,100)} />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
             </Helmet>
-            <h1>Hey I am from about page, inspect me</h1>
+            <h1>{aboutText}</h1>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 
 const Home = () => {
+    const [homeText] = useState('Now that we know who you are, I know who I am. Im not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villains going to be? Hes the exact opposite of the hero. And most times theyre friends, like you and me! I shouldve known way back when... You know why, David? Because of the kids. They called me Mr Glass.')
     return (
         <div>
             <Helmet>
                 <meta charSet="UTF-8" />
                 <title>I am from Home</title>
                 <link rel="canonical" href="http://localhost:3000/" />
-                <meta name="description" content="Now that we know who you are, I know who I am. I'm not a mistake! 
-                It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. 
-                And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. 
-                They called me Mr Glass." />
+                <meta name="description" content={homeText.slice(0, 100)} />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
             </Helmet>
-
-            <h1>I am from home comp!</h1>
+            <h1>{homeText}</h1>
         </div>
     )
 }
